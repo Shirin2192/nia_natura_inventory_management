c786e7069601ee9138f2c40c5f76a05c13c6eb6e
Shirin2192 <shirin@sda-zone.com>
2025-03-21 18:14:24 +0530

    add footer file in inventory folder view pages

--- application/views/inventory_manager/product.php
+++ application/views/inventory_manager/product.php
@@ -398,11 +398,8 @@
          </div>
       </div>
       <!-- Footer start -->
-      <div class="footer">
-         <div class="copyright">
-            <p>Copyright &copy; Designed & Developed by <a href="https://themeforest.net/user/quixlab">Quixlab</a> 2018</p>
-         </div>
-      </div>
+      <?php include('common/footer.php')?>
+      
       <!-- Footer end -->
       </div>
       <!-- Main wrapper end -->
