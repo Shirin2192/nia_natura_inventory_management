0c60f1c3fe5eea9981b3f3063a54b5580a4e58c2
shirin21 <ragbansinghshirin@gmail.com>
2025-04-05 14:52:59 +0530

    created change_sql file

--- database/change_sql.sql
+++ database/change_sql.sql
@@ -4,5 +4,10 @@ ALTER TABLE `tbl_product_inventory`
   DROP `fk_product_price_id`;
 
 
+ALTER TABLE `tbl_product_inventory` ADD `channel_type` VARCHAR(100) NULL DEFAULT NULL AFTER `fk_product_id`;
 
-  
+DROP TABLE `tbl_bottle_size`, `tbl_bottle_type`, `tbl_flavour`, `tbl_product`, `tbl_product_category`, `tbl_product_type`;
+
+ALTER TABLE `tbl_product_price`
+  DROP `fk_product_category_id`,
+  DROP `fk_product_type_id`;
